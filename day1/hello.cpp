eb17c02343e00f0e6fc0d9bef203460e5dd72f5f
raihanuldev <rihanthedev@gmail.com>
2024-11-16 23:27:08 +0600

    print,varibale,endl

---
+++ day1/hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    //varibale declaretion
+    int x =200;
+
+    std::cout << "Hello WorlD!";
+    //print varible there is no need to provide formate specifer
+
+    //for new line
+    std:: cout <<std::endl;
+    std :: cout <<x;
+    //print varibale with text 
+    std:: cout <<std::endl <<"my fav number is: "<<x;
+    return 0;
+}
