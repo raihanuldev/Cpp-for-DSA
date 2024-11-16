c9304801fa5d4772fe0da0e655de54cc7cb03b9d
raihanuldev <rihanthedev@gmail.com>
2024-11-16 23:40:51 +0600

    take input, typecasting

---
+++ day1/input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //take input
+    int age;
+    cin >> age;
+    cout <<age;
+    //get aschi value
+    int aschii = 'a';
+    cout <<" Aschi value " << aschii;
+
+    //typeCasting
+    cout <<" TypeCasting" <<(char)aschii;
+    return 0;
+}
