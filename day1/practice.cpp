58216d52e5a76f313f0579418d3171d26ee2b5f1
raihanuldev <rihanthedev@gmail.com>
2024-11-17 06:08:24 +0600

    reacp

---
+++ day1/practice.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout <<"Hello World! \n";
+    cout <<"Help me by giveing some information! \n";
+    int age;
+    cout << "WHat is your age? \n";
+    cin >> age;
+
+    cout <<"your age is "<<age <<" Years Old";
+    
+    return 0;
+}
