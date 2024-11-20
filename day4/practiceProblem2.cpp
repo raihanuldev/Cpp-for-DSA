d9d6371ba885ab4209d9e7c12daf22b29b401858
raihanuldev <rihanthedev@gmail.com>
2024-11-21 00:41:25 +0600

    added

---
+++ day4/practiceProblem2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Crickter {
+    public:
+    int jursy_no;
+    char country[20];
+};
+
+
+int main()
+{
+    Crickter dhoni;
+    dhoni.jursy_no = 123;
+    char[50] tem = "Bangldesh";
+    dhoni.country=tem;
+
+
+    return 0;
+}
