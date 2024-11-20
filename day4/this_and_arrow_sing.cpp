75f85cadfbc4140b41aa7833d031b89f1f0df9a0
raihanuldev <rihanthedev@gmail.com>
2024-11-20 07:50:07 +0600

    this

---
+++ day4/this_and_arrow_sing.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    int rol;
+    int year;
+    Student(int rol, int year)
+    {
+        this->rol = rol;
+        this->year = year;
+    }
+};
+
+int main()
+{
+    Student Rahim(789144, 2024);
+    cout << Rahim.rol;
+    return 0;
+}
+
+// this hocce ekta keyword |>
