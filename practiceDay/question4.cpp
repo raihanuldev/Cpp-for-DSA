2b5cd3a08e6e0c9e97d974b9d20530d92bfd0803
raihanuldev <rihanthedev@gmail.com>
2024-11-22 05:09:15 +0600

    add

---
+++ practiceDay/question4.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student{
+    public:
+    int id;
+    string name;
+    char section;
+    int totalMark;
+
+    Student(int id,string name,char section,int totalMark){
+        this->id = id;
+        this->name = name;
+        this->section = section;
+        this->totalMark = totalMark;
+    }
+    bool isBetterthan(Student other)
+    {
+        if(this->totalMark >other.totalMark) return true;
+        if(this->totalMark ==other.totalMark){
+            if(this->id <other.id) return true;
+        }
+        return false;
+    }
+};
+
+
+int main()
+{
+    int tcase;
+    cin >>tcase;
+    for(int i =0; tcase >=0; tcase--){
+        Student bestStudent(-1,"",' ',-1);
+        for(int i =0; i<3; i++){
+            int id, marks;
+            string name;
+            char section;
+
+            cin >> id >> name >> section >> marks;
+
+            Student currentStudent(id, name, section, marks);
+
+            if (currentStudent.isBetterthan(bestStudent)) {
+                bestStudent = currentStudent;
+            }
+        }
+         cout << bestStudent.id << " " << bestStudent.name << " " << bestStudent.section << " " << bestStudent.totalMark << endl;
+        
+    }
+    
+    return 0;
+}
