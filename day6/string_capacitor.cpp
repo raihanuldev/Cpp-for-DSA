04f09e8dbb56526ad669bd352b59924ab74edaa4
raihanuldev <rihanthedev@gmail.com>
2024-11-24 04:16:16 +0600

    string capacitor

---
+++ day6/string_capacitor.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Hello Worldweaewrwawrwa";
+    // s.clear();
+    s.resize(5);
+    // cout <<s.empty()<<endl;
+    cout << s.capacity()<<endl;
+    return 0;
+}
+
+// here is string capacitty
+//https://docs.google.com/document/d/1nxFrcoG_t-hMbeuAw6A5r_CV7I1lYvKLwEpupTQqkrM/edit?tab=t.0
