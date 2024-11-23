608b2aba75abffbeae0f6ddafdf18ba1901568ba
raihanuldev <rihanthedev@gmail.com>
2024-11-24 04:23:30 +0600

    Element Accessing useing buildin function

---
+++ day6/string_access.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Hello World";
+    cout <<s.at(1) << endl;
+    cout << s.front() <<endl;
+    cout <<s.back() <<endl;
+    cout << s[8] << endl; 
+    return 0;
+}
