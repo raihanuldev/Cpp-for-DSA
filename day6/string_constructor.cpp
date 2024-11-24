65e0dd27033506beb92d15a3f94f45c9092cc7e9
raihanuldev <rihanthedev@gmail.com>
2024-11-25 05:55:24 +0600

    StringConstructor

---
+++ day6/string_constructor.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{   
+    // string s = "Hello WOrld";
+    //1
+    // string s("Hello World");
+    //2
+    // string s("Hello World!",6);
+    //3
+    // string t = "Hello World!";
+    // string s(t,4);
+    //4
+    string s(5,'a');
+
+
+    cout <<s<<endl;
+    return 0;
+}
