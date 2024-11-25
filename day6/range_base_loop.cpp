7415fd742bb1b07ccb8449442b973266b3a4a445
raihanuldev <rihanthedev@gmail.com>
2024-11-25 06:06:08 +0600

    rangeBaseLoop

---
+++ day6/range_base_loop.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{   
+    string s;
+    cin >>s;
+    for(char c:s)
+    {
+        cout<<c;
+    }
+    // cout <<s<<endl;
+
+    return 0;
+}
