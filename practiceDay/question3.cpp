68630423d3069f7736b1a6e0d0f901a077d0c8d3
raihanuldev <rihanthedev@gmail.com>
2024-11-27 09:14:15 +0600

    addd

--- practiceDay/question3.cpp
+++ practiceDay/question3.cpp
@@ -17,32 +17,6 @@ int main() {
 }
 
 
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// int main() {
-//     string line;
-    
-//     // Read input until EOF
-//     while (getline(cin, line)) {
-//         stringstream ss(line); // Use stringstream to split words
-//         string word, result = "";
-        
-//         while (ss >> word) {
-//             sort(word.begin(), word.end()); // Sort each word
-//             result += word + " ";          // Append sorted word with a space
-//         }
-        
-//         if (!result.empty()) {
-//             result.pop_back(); // Remove the trailing space
-//         }
-        
-//         cout << result << endl;
-//     }
-
-//     return 0;
-// }
-
 
 
 
